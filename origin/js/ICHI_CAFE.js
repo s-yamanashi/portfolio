15094ddd40c94864e45e19e723707971b8437275
s-yamanashi <117365065+s-yamanashi@users.noreply.github.com>
2023-02-23 15:19:15 +0900

    Add files via upload

---
+++ origin/js/ICHI_CAFE.js
@@ -0,0 +1,15 @@
+$(function() {
+    /*-----スムーススクロール----*/
+    $('a[href^="#"]').click(function(){
+        let href= $(this).attr("href");
+        let target = $(href == "#" || href == "" ? 'html' : href);
+        let position = target.offset().top;
+        $("html, body").animate({scrollTop:position}, 600, "swing");
+        return false;
+    });
+
+    /*------ハンバーガー------*/
+    $('.btn').click(function(){
+        $('nav, .btn, .mask').toggleClass('open');
+    });
+})
